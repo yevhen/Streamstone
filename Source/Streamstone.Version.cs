8545f2f5f5a40d1bec47e58119670200973e3928
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2014-12-22 00:48:24 +0200

    Version

--- Source/Streamstone.Version.cs
+++ Source/Streamstone.Version.cs
@@ -2,5 +2,5 @@
 using System.Linq;
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
