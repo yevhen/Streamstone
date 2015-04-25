381513b3da8e5e4c1b66c33b0e15cdf518164673
Jakub Konecki <jkonecki@gmail.com>
2015-04-25 09:44:41 +0100

    Added example of writing to multiple streams in parallel for performance testing.

--- Source/Example/Scenarios/S04_Write_to_stream.cs
+++ Source/Example/Scenarios/S04_Write_to_stream.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Bson;
 
 using Streamstone;
+using System.Diagnostics;
 
 namespace Example.Scenarios
 {
@@ -14,6 +15,7 @@ namespace Example.Scenarios
         {
             WriteToExistingOrCreateNewStream();
             WriteSequentiallyToExistingStream();
+            WriteMultipleStreamsInParallel();
         }
 
         void WriteToExistingOrCreateNewStream()
@@ -50,7 +52,7 @@ namespace Example.Scenarios
                     Event(new InventoryItemCheckedIn(Partition, i*100)),
                 });
 
-                Console.WriteLine("Succesfully written event '{0}' under version '{1}'", 
+                Console.WriteLine("Succesfully written event '{0}' under version '{1}'",
                                    result.Events[0].Id, result.Events[0].Version);
 
                 Console.WriteLine("Etag: {0}, Version: {1}",
@@ -58,6 +60,31 @@ namespace Example.Scenarios
 
                 stream = result.Stream;
             }
+        }
+
+        void WriteMultipleStreamsInParallel()
+        {
+            Enumerable.Range(1, 50).AsParallel()
+                .ForAll(streamIndex =>
+                {
+                    var stream = new Stream(string.Concat(Partition, "-", streamIndex));
+                    Console.WriteLine("Writing to new stream in partition '{0}'", stream.Partition);
+                    var stopwatch = Stopwatch.StartNew();
+
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        var events = Enumerable.Range(1, 10)
+                            .Select(_ => Event(new InventoryItemCheckedIn(Partition, i * 1000 + streamIndex)))
+                            .ToArray();
+
+                        var result = Stream.Write(Table, stream, events);
+
+                        stream = result.Stream;
+                    }
+
+                    stopwatch.Stop();
+                    Console.WriteLine("Finished writing 300 events to new stream in partition '{0}' in {1}ms", stream.Partition, stopwatch.ElapsedMilliseconds);
+                });
         }
 
         static Event Event(object e)
