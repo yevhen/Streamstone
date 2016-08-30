f2349b89f6e28307cc9c6d2b40ff86a7a1acae4c
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2016-08-31 00:58:12 +0300

    Bump version

--- Source/Streamstone.Version.cs
+++ Source/Streamstone.Version.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.9.9.0")]
-[assembly: AssemblyFileVersion("0.9.9.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
