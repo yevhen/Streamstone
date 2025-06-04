a40555c7a9a091157ee9bff45cbd711922389891
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2025-06-05 02:22:24 +0300

    Update Docusaurus base URL for deployment to Streamstone subdirectory

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://yevhen.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Streamstone/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
