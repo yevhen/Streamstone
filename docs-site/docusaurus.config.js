7bab3f0ae2ffd108046abc8ae955bb29ee676233
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2025-06-05 01:41:31 +0300

    Update Docusaurus configuration for GitHub Pages deployment

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://streamstone.cloud',
+  url: 'https://yevhen.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Streamstone/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
